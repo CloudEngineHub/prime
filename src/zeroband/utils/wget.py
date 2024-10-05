8a701318e6bae66bde1d06b0b0ba5e33f0ca51e1
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2024-10-05 18:40:38 +0000

    add error msg wget

--- src/zeroband/utils/wget.py
+++ src/zeroband/utils/wget.py
@@ -1,11 +1,15 @@
 import subprocess
 
 from zeroband.utils.logging import get_logger
+import shutil
 
 
 def wget(source: str, destination: str) -> None:
     logger = get_logger()
     cmd = f"wget -r -np -nH --cut-dirs=1 -P {destination} {source}"
+
+    if shutil.which("wget") is None:
+        raise RuntimeError("wget is required but not found. Please install wget and try again.")
 
     try:
         subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)

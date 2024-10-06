2e0fcb7126ac94e069773ab7bb4ca6b153bcb4eb
Jackmin801 <ongjackm@gmail.com>
2024-10-06 10:43:27 +0800

    template for pgGloo

--- src/zeroband/C/collectives.py
+++ src/zeroband/C/collectives.py
@@ -4,14 +4,14 @@ import torch.distributed as dist
 from torch.utils import cpp_extension
 from pathlib import Path
 
-INCLUDES = [str(Path(__file__).parent / "csrc")]
+INCLUDES = [str(Path(__file__).parent / "csrc"), "/home/jackmin/Documents/ZeroBand/third_party/gloo"]
 COLLECTIVES_CSRC_PATH = Path(__file__).parent / "csrc" / "collectives.cpp"
 
 collectives_ops = cpp_extension.load(
     name="collectives",
     sources=[COLLECTIVES_CSRC_PATH],
-    extra_cflags=["-O3"],
-    verbose=False,
+    extra_cflags=["-O3", "-DUSE_C10D_GLOO"],
+    verbose=True,
     extra_include_paths=INCLUDES,
 )
 
@@ -25,4 +25,7 @@ def ring_allreduce(
 ) -> None:
     if group is None:
         group = dist.distributed_c10d._get_default_group()
-    collectives_ops.ring_allreduce(tensor, op, group)
+    if isinstance(group, dist.distributed_c10d.ProcessGroupGloo):
+        collectives_ops.ring_allreduce_gloo(tensor, op, group)
+    else:
+        collectives_ops.ring_allreduce(tensor, op, group)

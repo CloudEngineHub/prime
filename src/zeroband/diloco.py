6508b54ea6b34ce257cb0bb49b0509decfee0fc4
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2024-10-11 01:40:45 +0000

    add more logging all reduce bucket

--- src/zeroband/diloco.py
+++ src/zeroband/diloco.py
@@ -85,7 +85,7 @@ class Diloco:
         """
         Sync the pseudo gradient from the local process group to the global process group
         """
-        _start_time = time.time()
+        _start_time = time.perf_count()
         self._logger.debug("sync pseudo gradient %s", " fake" if fake else "")
 
         self.elastic_device_mesh.maybe_reinit_global_pg(admit_joiners=False)
@@ -99,15 +99,20 @@ class Diloco:
                     param_offloaded.grad.to_local().sub_(param.data.to_local().to(param_offloaded.data.device))
             try:
                 self.offloaded_grad_flat_tensor.div_(global_pg.size())
-                _collective_start_time = time.time()
+                _collective_start_time = time.perf_count()
 
                 self._logger.debug("Beginning all reduce")
                 global_pg.monitored_barrier()
                 # all_reduce(self.config.compression, self.offloaded_grad_flat_tensor, dist.ReduceOp.SUM, global_pg)
-                for tensor_group in self._offloaded_grad_grouped_tensor:
+                for j, tensor_group in enumerate(self._offloaded_grad_grouped_tensor):
+                    t0 = time.perf_count()
                     all_reduce(self.config.compression, tensor_group, dist.ReduceOp.SUM, global_pg)
+                    print(
+                        f"{j}/{len(self._offloaded_grad_grouped_tensor)} all reduce bucket done in {time.perf_count() - t0:.6f} seconds"
+                    )
+
                 self._logger.debug(
-                    f"All reduce takes {time.time() - _collective_start_time:.6f} seconds numels: {self.offloaded_grad_flat_tensor.numel()}"
+                    f"All reduce takes {time.perf_count() - _collective_start_time:.6f} seconds numels: {self.offloaded_grad_flat_tensor.numel()}"
                 )
                 break
             except Exception as e:
@@ -125,7 +130,7 @@ class Diloco:
                     param_offloaded.grad.to_local().copy_(param_offloaded.data.to_local())
                     param_offloaded.grad.to_local().sub_(param.data.to_local().to(param_offloaded.data.device))
 
-        self._logger.info(f"Sync psuedo-gradient in {time.time() - _start_time:.6f} seconds")
+        self._logger.info(f"Sync psuedo-gradient in {time.perf_count() - _start_time:.6f} seconds")
 
     def sync_inner_model(self, model: nn.Module):
         """
